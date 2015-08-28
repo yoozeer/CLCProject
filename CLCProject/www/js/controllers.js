f4c722063d28a4f07396e6c19f89cf60d04a9597
yoozeer <yusuf.oozeer@gmail.com>
2015-08-28 11:00:14 +0400

    added map feature

--- CLCProject/www/js/controllers.js
+++ CLCProject/www/js/controllers.js
@@ -13,8 +13,38 @@ angular.module('starter.controllers', [])
   $scope.course = Courses.get($stateParams.courseId);
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+.controller('AccountCtrl', function($scope, $http) {
+
+})
+
+
+.controller('MapController', function($scope, $ionicLoading) {
+
+        var myLatlng = new google.maps.LatLng(-20.259861, 57.484503);
+        var mapOptions = {
+            center: myLatlng,
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+
+        };
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+        var myLocation = new google.maps.Marker({
+                position: new google.maps.LatLng(-20.259861, 57.484503),
+                map: map,
+                title: "Ceridian Learning Centre"
+            });
+
+
+       // navigator.geolocation.getCurrentPosition(function(pos) {
+            //map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+            //var myLocation = new google.maps.Marker({
+               // position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+               // map: map,
+               // title: "My Location"
+           // });
+       // });
+        $scope.map = map;
+
+})
+;
